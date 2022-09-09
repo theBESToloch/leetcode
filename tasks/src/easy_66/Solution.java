f3e4148d579ca73a26f01d5f469d89e7d29c4acd
pavelzubkov <pavelzubkov1995@yandex.ru>
2022-09-09 16:57:03 +0300

    lead code solutions

--- tasks/src/easy_66/Solution.java
+++ tasks/src/easy_66/Solution.java
@@ -6,31 +6,17 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int length = digits.length;
 
-        int i = length - 1;
-        int cur = digits[i] + 1;
-
-        digits[i] = cur % 10;
-        cur = cur / 10;
-
-        if (cur == 0) {
-            return digits;
-        }
-
-        while (cur > 0) {
-            i--;
-            if (i >= 0) {
-                cur = digits[i] + cur;
-                digits[i] = cur % 10;
-                cur = cur / 10;
-            } else {
-                int[] arr = new int[length +1];
-                System.arraycopy(digits, 0, arr, 1, length);
-                arr[0] = 1;
-                return arr;
-                //rerange array, copy and put first number - 1
+        for (int i = length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
+            digits[i] = 0;
         }
-        return digits;
+
+        int[] arr = new int[length + 1];
+        arr[0] = 1;
+        return arr;
     }
 
     public static void main(String[] args) {
